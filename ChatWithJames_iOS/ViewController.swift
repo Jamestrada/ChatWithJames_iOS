12c49bbb23ada42f9fb2fd746e49b751b0771b32
James Estrada <Jamestrada@users.noreply.github.com>
2021-02-05 00:43:00 -0600

    Initial Commit

---
+++ ChatWithJames_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatWithJames_iOS
+//
+//  Created by James Estrada on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
