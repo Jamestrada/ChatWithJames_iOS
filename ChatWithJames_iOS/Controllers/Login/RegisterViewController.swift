5b1ea5a70f2a7d73d5ed3934d51b5db4ca509027
James Estrada <Jamestrada@users.noreply.github.com>
2021-02-11 23:55:48 -0600

    add action sheet to present options for profile pic

--- ChatWithJames_iOS/Controllers/Login/RegisterViewController.swift
+++ ChatWithJames_iOS/Controllers/Login/RegisterViewController.swift
@@ -124,7 +124,7 @@ class RegisterViewController: UIViewController {
     }
     
     @objc private func didTapChangeProfilePic() {
-        print("Change pic called")
+        presentPhotoActionSheet()
     }
     
     override func viewDidLayoutSubviews() {
@@ -185,5 +185,24 @@ extension RegisterViewController: UITextFieldDelegate {
             registerButtonTapped()
         }
         return true
+    }
+}
+
+extension RegisterViewController: UIImagePickerControllerDelegate {
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: {_ in }))
+        actionSheet.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: {_ in }))
+        
+        present(actionSheet, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
     }
 }
