727a10a050d8ec01d758cd31171f1178205be510
James Estrada <Jamestrada@users.noreply.github.com>
2021-02-16 13:46:11 -0600

    add firebase login and registration

--- ChatWithJames_iOS/Controllers/Login/LoginViewController.swift
+++ ChatWithJames_iOS/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -106,6 +107,14 @@ class LoginViewController: UIViewController {
         }
         
         // Firebase login
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to log in user with email: \(email)")
+                return
+            }
+            let user = result
+            print("Logged In User: \(user)")
+        })
     }
     
     func alertUserLoginError() {
