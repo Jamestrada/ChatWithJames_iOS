07a37375788747595aef8186980657b48b767ebf
James Estrada <Jamestrada@users.noreply.github.com>
2021-02-17 16:20:48 -0600

    bug fix registering email address

--- ChatWithJames_iOS/Resources/DatabaseManager.swift
+++ ChatWithJames_iOS/Resources/DatabaseManager.swift
@@ -19,7 +19,9 @@ final class DatabaseManager {
 extension DatabaseManager {
     // check if user exists, the same email can't be shared with more than one user
     public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        let safeEmail = email.replacingOccurrences(of: ".", with: "-")
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -30,7 +32,7 @@ extension DatabaseManager {
     
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         ])
@@ -41,5 +43,10 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        let safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        return safeEmail
+    }
 //    let profilePictureUrl: String
 }
