7a543f886f1aef29de051821cbe28881784f17b2
James Estrada <Jamestrada@users.noreply.github.com>
2021-02-17 16:02:37 -0600

    add database

---
+++ ChatWithJames_iOS/Resources/DatabaseManager.swift
@@ -0,0 +1,45 @@
+//
+//  DatabaseManager.swift
+//  ChatWithJames_iOS
+//
+//  Created by James Estrada on 2/17/21.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+}
+
+// MARK: - Account Management
+
+extension DatabaseManager {
+    // check if user exists, the same email can't be shared with more than one user
+    public func userExists(with email: String, completion: @escaping ((Bool) -> Void)) {
+        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        })
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.emailAddress).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+//    let profilePictureUrl: String
+}
