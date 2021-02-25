5a8ee9b9e415cf3cf1cf8a028c78a26894ccda4d
James Estrada <Jamestrada@users.noreply.github.com>
2021-02-24 23:59:08 -0600

    fetch user's profile picture from fb and google after signing in

--- ChatWithJames_iOS/Resources/StorageManager.swift
+++ ChatWithJames_iOS/Resources/StorageManager.swift
@@ -24,9 +24,27 @@ final class StorageManager {
         storage.child("images/\(fileName)").putData(data, metadata: nil, completion: { metadata, error in
             guard error == nil else {
                 // failed
-                completion(.failure(<#T##Error#>))
+                print("Failed to upload data to firebase for picture")
+                completion(.failure(StorageErrors.failedToUpload))
                 return
             }
+            
+            self.storage.child("images/\(fileName)").downloadURL(completion: { url, error in
+                guard let url = url else {
+                    print("Failed to get download url")
+                    completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("Download url returned: \(urlString)")
+                completion(.success(urlString))
+            })
         })
+    }
+    
+    public enum StorageErrors: Error {
+        case failedToUpload
+        case failedToGetDownloadUrl
     }
 }
